67ab54a36f2d96e93a3422aa057e171264ee2659
Juliana Flávia Vitor <juliana26ads@gmail.com>
2026-06-14 17:14:23 -0300

    Meu primeiro código Java

---
+++ JulianaFlavia.java
@@ -0,0 +1,5 @@
+*public class JulianaFlavia {
+    public static void main(String[] args) {
+        System.out.println("Eis uma nova desenvolvidora de Sistemas. Muito prazer, sou Juliana Flávia.");
+        }
+}
